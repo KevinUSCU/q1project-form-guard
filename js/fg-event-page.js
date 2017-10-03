b4f8616d07e9a256bc510cf8b494f25f7f59dbbb
Kevin Springer <KevinUSCU@outlook.com>
2017-10-02 22:56:28 -0700

    Move getSavedFormData and saveFormData into fg-event-page.js. Add deleteFormData function to fs-event-page.js. Add more comprehensive onMessage listener to fg-event-page.js.

--- js/fg-event-page.js
+++ js/fg-event-page.js
@@ -1,11 +1,58 @@
-// Listener for Injector to enable extension
+// Listener for messages from Injector
+// (message is an array where index 0 is the command, and index 1 carries data)
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message === "enable") {
+    if (message[0] === "enable") {
         // Enable extension button
         chrome.pageAction.show(sender.tab.id);
-        // Pass url back to tab
-        sendResponse(sender.tab.url);
+        // Check for previously stored data
+        getSavedFormData(sender.tab.url, (data) => {
+            if (data) {
+                let response = [true, data];
+            } else {
+                let response = [false, null];
+            }
+            // Pass existence of stored data back to tab
+            sendResponse(response);
+        });
+    } else if (message[0] === "fetch") {
+        // Serve request to fetch previously stored data
+        getSavedFormData(sender.tab.url, (data) => {
+            if (data) {
+                let response = [true, data];
+            } else {
+                let response = [false, null];
+            }
+            // Pass existence of stored data back to tab
+            sendResponse(response);
+        });
+    } else if (message[0] === "save") {
+        // Serve request to save data
+        saveFormData(sender.tab.url, message[1]);
+    } else if (message[0] === "delete") {
+        // Serve request to delete data for this page
+        deleteFormData(sender.tab.url);
     }
 })
+
+// Function to read saved data from storage
+function getSavedFormData(url, callback) {
+    // chrome.runtime.lastError is true if item cannot be retrieved from storage
+    chrome.storage.local.get(url, (items) => {
+        callback(chrome.runtime.lastError ? null : items[url]);
+    });
+}
+
+// Function to save form data to storage
+function saveFormData(url, data) {
+    var items = {};
+    items[url] = data;
+    chrome.storage.local.set(items);
+}
+
+// Function to delete form data from storage
+function deleteFormData(url) {
+    chrome.storage.local.remove(url);
+}
+
 
 var kevin = "hi there!";
